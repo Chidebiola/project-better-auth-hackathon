8e91ad2250be87f1f334d688c67fcd6c621e11b2
Natt <natt.lefevre@gmail.com>
2026-02-07 14:27:59 -0800

    Add @biomejs/biome dependency and update VSCode settings for formatting; refactor Logomark and Logotype components with new SVGs

--- components/logomark.tsx
+++ components/logomark.tsx
@@ -1,23 +1,14 @@
 import type { ComponentProps } from "react"
 
-interface LogomarkProps extends ComponentProps<"svg"> {}
+interface LogomarkProps extends ComponentProps<"svg"> { }
 
 export function Logomark(props: LogomarkProps) {
   const { ...rest } = props
 
   return (
-    <svg
-      fill="currentColor"
-      height="18"
-      viewBox="0 0 24 18"
-      width="24"
-      xmlns="http://www.w3.org/2000/svg"
-      {...rest}
-    >
-      <title>Logomark</title>
-      <path d="M18 0H24V6H18V0Z" />
-      <path d="M22.6298 11.1117C23.5344 13.2956 24 15.6362 24 18H18C18 16.4241 17.6896 14.8637 17.0866 13.4078C16.4835 11.9519 15.5996 10.629 14.4853 9.51472C13.371 8.40042 12.0481 7.5165 10.5922 6.91345C9.13629 6.31039 7.57586 6 6 6V0C8.36379 0 10.7044 0.465584 12.8883 1.37017C15.0722 2.27475 17.0565 3.60062 18.7279 5.27208C20.3994 6.94353 21.7252 8.92784 22.6298 11.1117Z" />
-      <path d="M0 6H6V18H0V6Z" />
+    <svg width="28" height="21" viewBox="0 0 28 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M-6.11959e-07 7C7.73199 7 14 13.268 14 21C14 13.268 20.268 7 28 7C28 14.732 21.732 21 14 21C6.26801 21 -2.73984e-07 14.732 -6.11959e-07 7Z" fill="#12A594" />
+      <path d="M7 7C10.866 7 14 3.86599 14 -7.01366e-07C14 3.86599 17.134 7 21 7C17.134 7 14 10.134 14 14C14 10.134 10.866 7 7 7Z" fill="#12A594" />
     </svg>
   )
 }
